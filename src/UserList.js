294f68555e95f7c8aa3c5ca745262a0d8c17c31c
Bill Walker <bill@raythree.com>
2019-10-06 08:32:14 -0400

    fixed useEffect dependency

--- src/UserList.js
+++ src/UserList.js
@@ -1,11 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import MuiTable from 'material-table';
+import React, { useState, useEffect } from "react";
+import MuiTable from "material-table";
 
-export default (props) => {
-  const title="Controlled Selection"
-  const columns=[
-    { title: 'First', field: 'first' },
-    { title: 'Last', field: 'last' }
+function getTableData(props) {
+  return props.data.map(row => {
+    return {
+      ...row,
+      tableData: { checked: props.selection[row.id] ? true : false }
+    };
+  });
+}
+
+export default props => {
+  const title = "Controlled Selection";
+  const columns = [
+    { title: "First", field: "first" },
+    { title: "Last", field: "last" }
   ];
   const options = {
     search: false,
@@ -15,7 +24,7 @@ export default (props) => {
 
   function onChangeSelection(rows) {
     let newSelection = {};
-    rows.forEach((row) => {
+    rows.forEach(row => {
       if (row.tableData.checked) {
         newSelection[row.id] = true;
       }
@@ -23,24 +32,18 @@ export default (props) => {
     props.onChangeSelection(newSelection);
   }
 
-  function getTableData() {
-    return props.data.map((row) => {
-      return {...row, tableData: {checked: props.selection[row.id] ? true: false}};
-    })
-  }
-
-  let [tableData, setTableData] = useState(getTableData());
+  let [tableData, setTableData] = useState(getTableData(props));
   useEffect(() => {
-    setTableData(getTableData());
-  }, [props.selection, props.data]);
+    setTableData(getTableData(props));
+  }, [props]);
 
-  return (    
-    <MuiTable 
-      title={title} 
-      columns={columns} 
-      data={tableData} 
+  return (
+    <MuiTable
+      title={title}
+      columns={columns}
+      data={tableData}
       options={options}
       onSelectionChange={onChangeSelection}
     />
   );
-}
+};
